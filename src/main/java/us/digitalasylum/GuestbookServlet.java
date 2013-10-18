c9b4b6d2b61523462c568610f228e804338dd3f1
nuts420@gmail.com <>
2013-10-18 21:30:46 +0000

    adding guestbook servlet

---
+++ src/main/java/us/digitalasylum/GuestbookServlet.java
@@ -0,0 +1,18 @@
+package us.digitalasylum;
+
+import java.io.IOException;
+//import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GuestbookServlet extends HttpServlet
+{
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("Hello, world");
+    }
+}
